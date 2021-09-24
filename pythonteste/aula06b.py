c6f05b92fddc51a7b15712b6afeb0935d4a368af
Rafael <rafael_meyer_@hotmail.com>
2021-09-24 18:57:14 -0300

    Aula 06 - Tipos Primitivos e Saída de Dados

---
+++ pythonteste/aula06b.py
@@ -0,0 +1,6 @@
+n = (input('Digite algo: '))
+print ('Você digitou um número? {}'.format(n.isnumeric()))
+print ('Você digitou uma palavra? {}'.format(n.isalpha()))
+print ('Você digitou um alfanumérico {}'.format(n.isalnum()))
+print ('Você digitou tudo maiúsculo? {}'.format(n.isupper()))
+
