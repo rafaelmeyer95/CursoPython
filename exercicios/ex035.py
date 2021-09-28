c064ff4c4ffea9f38fbdd42b78b4c9fc8b925946
Rafael <rafael_meyer_@hotmail.com>
2021-09-28 09:52:48 -0300

    Analisando Triângulo v1.0

---
+++ exercicios/ex035.py
@@ -0,0 +1,10 @@
+# Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
+
+a = float(input('Digite o comprimento do primeiro vértice: '))
+b = float(input('Digite o comprimento do segundo vértice: '))
+c = float(input('Digite o comprimento do terceiro vértice: '))
+
+if a + b < c or a + c < b or b + c < a:
+    print('Os valores indicados não podem formar um triângulo')
+else:
+    print('Os valores indicados podem formar um triângulo')
