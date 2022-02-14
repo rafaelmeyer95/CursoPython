15b11dd122ece9021c6f8e12da464eb2cf82878f
Rafael <rafael_meyer_@hotmail.com>
2022-02-14 11:38:00 -0300

    Exercício 107 - Exercitando módulos em Python

---
+++ exercicios/ex107/ex107.py
@@ -0,0 +1,9 @@
+# Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade().
+# Faça também um programa que importe esse módulo e uso algumas dessas funções
+
+import moeda
+
+preco = int(input("Digite o preço: "))
+print(f'A metade de R${preco} é R${moeda.metade(preco)}')
+print(f'O dobro de R${preco} é R${moeda.dobro(preco)}')
+print(f'Aumentando 10%, temos R${moeda.aumentar(preco,10)}')
---
+++ exercicios/ex107/moeda.py
@@ -0,0 +1,15 @@
+def metade(p):
+    res = p/2
+    return res
+
+def dobro(p):
+    res = p*2
+    return res
+
+def diminuir(preco, descont):
+    res = preco - preco*descont/100
+    return res
+
+def aumentar(preco, aument):
+    res = preco + preco*aument/100
+    return res
